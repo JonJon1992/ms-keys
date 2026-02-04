343fb978c4becd8b575dbdb1bdef502d259e460a
YOUR NAME <CHANGE_ME@example.com>
2026-02-04 13:26:23 -0300

    chore: update composer.json to set minimum stability to dev
    - Adjusted the minimum stability in composer.json to allow for development versions of dependencies, facilitating the use of pre-release packages.

--- public/index.php
+++ public/index.php
@@ -3,10 +3,9 @@
 use Core\App\App;
 
 require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../vendor/jonjon1992/php-slim-modular/src/bootstrap.php';
 
 /**
  * @var  App $app
  */
-$app = require __DIR__ . '/../vendor/jonjon1992/php-slim-modular/src/bootstrap.php';
-
 $app->run();
